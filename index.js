8a852b81b84e310ff935bb3e903650ec81aa04e4
Pericles Narbal <pericles.oliveira@ciccr-ce.interno>
2021-04-04 19:40:49 -0300

    quase concluido turno

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.use("/", (req, res) => {
 // });
 
 var chatUsers = {}
+var tabuleiro = createMatrix()
+var coresDisponiveis = ["branco", "preto"]
 
 socketServer.on('connect', (socket) => {
   const socketId = socket.id
@@ -29,6 +31,8 @@ socketServer.on('connect', (socket) => {
     chatUsers[socketId] = usern
     console.log(`[CONNECTION]: O usuário ${chatUsers[socketId]} se conectou: ${socketId}`)
     socketServer.emit('chat message', `${chatUsers[socketId]} entrou na sala...`)
+    socket.emit("tabuleiro", tabuleiro);
+    socket.emit("cores disponiveis", coresDisponiveis)
   })
   
   socket.on('disconnect', () => {
@@ -41,8 +45,33 @@ socketServer.on('connect', (socket) => {
     console.log(`[MESSAGE]: ${chatUsers[socketId]}: ${msg}`);
     socketServer.emit('chat message', `${chatUsers[socketId]}: ${msg}`);
   });
+
+  socket.on("cor", cor => {
+    console.log("SOCKET ON COR")
+    var status = coresDisponiveis.indexOf(cor);
+    if (status != -1){
+      coresDisponiveis.splice(status, 1);
+    }
+    socketServer.emit("cores disponiveis", coresDisponiveis)
+  })
 });
 
 server.listen(3000, () => {
   console.log(`Socket.IO server running at http://localhost:3000/`);
 });
+
+function createMatrix(){
+  var tabuleiro = []
+  for(i = 1; i <= 8; i++){
+    tabuleiro[i] = new Array();
+    for(j = 1; j <= 8; j++){
+      tabuleiro[i][j] = new Array();
+      tabuleiro[i][j] = 'semBolinha';
+    }
+  }
+  tabuleiro[4][4] = 'branco';
+  tabuleiro[4][5] = 'preto';
+  tabuleiro[5][4] = 'preto';
+  tabuleiro[5][5] = 'branco';
+  return tabuleiro;
+}
